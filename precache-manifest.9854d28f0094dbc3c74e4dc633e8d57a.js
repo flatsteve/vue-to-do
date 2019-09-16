f97183de9f3a96ea0b65cce5387608f931e8b2cb
Stephen McCann <info@flatsteve.com>
2019-09-16 11:37:06 +0100

    Release

---
+++ precache-manifest.9854d28f0094dbc3c74e4dc633e8d57a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bca0acfdcaf30b6b3429",
+    "url": "/vue-to-do/js/about.9c4c5747.js"
+  },
+  {
+    "revision": "5c48f9263d2c2b2f5758",
+    "url": "/vue-to-do/css/app.a89590a2.css"
+  },
+  {
+    "revision": "5c48f9263d2c2b2f5758",
+    "url": "/vue-to-do/js/app.b60ffe66.js"
+  },
+  {
+    "revision": "303a06caed03a092d9d1",
+    "url": "/vue-to-do/css/chunk-vendors.20623606.css"
+  },
+  {
+    "revision": "303a06caed03a092d9d1",
+    "url": "/vue-to-do/js/chunk-vendors.c7a21011.js"
+  },
+  {
+    "revision": "ebc517cd7b82230baf53f6b3c39b6c37",
+    "url": "/vue-to-do/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-to-do/robots.txt"
+  }
+];
