52aab91b1d8698ea2c260f0ad2c6851fcc78983d
Stephen McCann <info@flatsteve.com>
2019-09-16 11:30:05 +0100

    Release

---
+++ precache-manifest.52f60fb02ac65231e4fa6302fefb49d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bca0acfdcaf30b6b3429",
+    "url": "/js/about.9c4c5747.js"
+  },
+  {
+    "revision": "57dc4c7b4c79c4e1d61b",
+    "url": "/css/app.a89590a2.css"
+  },
+  {
+    "revision": "57dc4c7b4c79c4e1d61b",
+    "url": "/js/app.320ee107.js"
+  },
+  {
+    "revision": "303a06caed03a092d9d1",
+    "url": "/css/chunk-vendors.20623606.css"
+  },
+  {
+    "revision": "303a06caed03a092d9d1",
+    "url": "/js/chunk-vendors.c7a21011.js"
+  },
+  {
+    "revision": "b6a428ca420c644380dd121ba70e714e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
