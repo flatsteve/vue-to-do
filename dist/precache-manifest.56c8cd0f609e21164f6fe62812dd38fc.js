0f45950633c231a0d5ff6816bf4e7a7de77d512c
Stephen McCann <info@flatsteve.com>
2019-09-18 12:46:43 +0100

    Release

---
+++ dist/precache-manifest.56c8cd0f609e21164f6fe62812dd38fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "07e42d068f932cf15384",
+    "url": "/vue-to-do/css/app.4781d267.css"
+  },
+  {
+    "revision": "07e42d068f932cf15384",
+    "url": "/vue-to-do/js/app.18fdb87a.js"
+  },
+  {
+    "revision": "7853867b57c5504b01cf",
+    "url": "/vue-to-do/css/chunk-vendors.20623606.css"
+  },
+  {
+    "revision": "7853867b57c5504b01cf",
+    "url": "/vue-to-do/js/chunk-vendors.ad87b27a.js"
+  },
+  {
+    "revision": "f4ab7aee1b8ff6c6f37e28ef2ff31c99",
+    "url": "/vue-to-do/index.html"
+  },
+  {
+    "revision": "70ce796a6a13ba47b37f720b895d5794",
+    "url": "/vue-to-do/svg/bin.svg"
+  },
+  {
+    "revision": "08d8b5031b2e8edff94e606603a8fd81",
+    "url": "/vue-to-do/svg/loading.svg"
+  },
+  {
+    "revision": "4d8fce12006d42cdeef140fb3b2cc431",
+    "url": "/vue-to-do/404.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-to-do/robots.txt"
+  }
+];
