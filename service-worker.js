67ea16aa7ddd9c557ec465704664c2f41c31d101
Stephen McCann <info@flatsteve.com>
2019-09-25 20:17:09 +0100

    Release

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vue-to-do/precache-manifest.616bc32f54f396030ac8bb20fba604d9.js"
+  "/vue-to-do/precache-manifest.7ea170a24e61c4775d1d8bc217b50fab.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-to-do"});
